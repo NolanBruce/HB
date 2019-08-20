c9e20ae63375bf4acbcfdf950ebe852980199653
nmbruce <nmbruce@asu.edu>
2019-08-19 20:24:33 -0400

    Initial Commit

---
+++ HBApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  HBApp
+//
+//  Created by Nolan Bruce on 8/19/19.
+//  Copyright © 2019 Nolan Bruce. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
