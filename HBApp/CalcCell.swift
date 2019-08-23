47c8d57c8f2fa9897f0ca6c2822359333706b8b3
nmbruce <nmbruce@asu.edu>
2019-08-22 22:08:39 -0400

    Initial commit. Contains a few views with no legitimate functionality implemented

---
+++ HBApp/CalcCell.swift
@@ -0,0 +1,13 @@
+//
+//  CalcCell.swift
+//  HBApp
+//
+//  Created by Nolan Bruce on 8/19/19.
+//  Copyright © 2019 Nolan Bruce. All rights reserved.
+//
+
+import UIKit
+
+protocol CalcCellDelegate {
+    func onTapCalc(
+}
